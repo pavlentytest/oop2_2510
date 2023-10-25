4d29e0aa8729e29df7045bf2ab6ba6aef20fb576
Paul <impetus@yandex.ru>
2023-10-25 18:15:16 +0300

    Add full class

--- src/dev2/Main.java
+++ src/dev2/Main.java
@@ -1,12 +1,22 @@
 package dev2;
-
-
 import dev1.Car;
-
 import java.util.*;
+
+class Test {
+
+}
+class MegaTest extends Test {
+
+}
+
 
 public class Main {
     public static void main(String[] args) {
+
+        Test test1 = new Test();
+
+
+
         Scanner scanner = new Scanner(System.in);
         Car car1 = new Car();
         car1.wheels = 6;
---
+++ src/dev3/Vector2D.java
@@ -0,0 +1,54 @@
+package dev3;
+
+public class Vector2D {
+    public double vX;
+    public double vY;
+    public static int count = 0;
+
+    public Vector2D() {
+        this.vX = 1.0;
+        this.vY = 1.0;
+        count++;
+    }
+    public Vector2D(double x, double y) {
+        this.vX = x;
+        this.vY = y;
+    }
+
+    public Vector2D(Vector2D v2d) {
+        this.vX = v2d.vX;
+        this.vY = v2d.vY;
+    }
+
+    public void print() {
+        System.out.printf("(%.2f, %.2f)\n",vX,vY);
+    }
+
+    public double length() {
+        return Math.sqrt(vX*vX + vY*vY);
+    }
+
+    public void add(Vector2D newvector) {
+        this.vX = this.vX + newvector.vX;
+        this.vY = this.vY + newvector.vY;
+    }
+    public void sub(Vector2D newvector) {
+        this.vX -= newvector.vX;
+        this.vY -= newvector.vY;
+    }
+
+    public void scale(double scaleFactor) {
+        this.vX = this.vX * scaleFactor;
+        this.vY = this.vY * scaleFactor; // this.vY *= scaleFactor;
+    }
+
+    public double dotProduct(Vector2D v) {
+        return this.vX*v.vX + this.vY*v.vY;
+    }
+
+    public void normalized() {
+        double length = length();
+        this.vX /= length;
+        this.vY /= length;
+    }
+}
